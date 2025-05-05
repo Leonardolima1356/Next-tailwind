bea287cfcb75a5a3b086fdda3dd1c8a18e2749f2
Leonardo Lima <leonardolima1356@gmail.com>
2025-05-05 15:57:10 -0300

    Add files via upload

---
+++ src/app/axiosPage/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { useEffect, useState, Suspense } from "react"
+import Imagem from "next/image"
+
+const AxiosPage = () => {
+    return(
+        <>
+
+        </>
+    )
+}
+
+export default AxiosPage
